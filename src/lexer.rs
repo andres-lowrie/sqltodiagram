64af9318f14db4851ba7f64631a253d1f80a7220
Andres Lowrie <andres.lowrie@gmail.com>
2019-07-04 11:53:38 -0500

    Add operator tokens

--- src/lexer.rs
+++ src/lexer.rs
@@ -33,12 +33,46 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_can_return_correct_tokens() {
+    fn it_can_return_operator_tokens() {
         let want = vec![
             token::Token::new(token::TokenType::Plus),
-            token::Token::new(token::TokenType::Minus),
+            token::Token::new(token::TokenType::Asterisk),
+            token::Token::new(token::TokenType::Slash),
+            token::Token::new(token::TokenType::LeftAngle),
+            token::Token::new(token::TokenType::RightAngle),
+            token::Token::new(token::TokenType::Equals),
+            token::Token::new(token::TokenType::Tilde),
+            token::Token::new(token::TokenType::Bang),
+            token::Token::new(token::TokenType::At),
+            token::Token::new(token::TokenType::Hash),
+            token::Token::new(token::TokenType::Percent),
+            token::Token::new(token::TokenType::Caret),
+            token::Token::new(token::TokenType::Ampersand),
+            token::Token::new(token::TokenType::Pipe),
+            token::Token::new(token::TokenType::Backtick),
+            token::Token::new(token::TokenType::Question),
+            token::Token::new(token::TokenType::Hyphen),
+            token::Token::new(token::TokenType::Dollar),
+            token::Token::new(token::TokenType::LeftParen),
+            token::Token::new(token::TokenType::RightParen),
+            token::Token::new(token::TokenType::LeftBracket),
+            token::Token::new(token::TokenType::RightBracket),
+            token::Token::new(token::TokenType::Comma),
+            token::Token::new(token::TokenType::Semicolon),
+            token::Token::new(token::TokenType::Colon),
+            token::Token::new(token::TokenType::Period),
         ];
-        let got = get_tokens("+-");
+        let got = get_tokens("+*/<>=~!@#%^&|`?-$()[],;:.");
+        assert_eq!(want, got);
+    }
+
+    #[test]
+    fn it_can_return_quotes() {
+        let want = vec![
+            token::Token::new(token::TokenType::SingleQuote),
+            token::Token::new(token::TokenType::DoubleQuote),
+        ];
+        let got = get_tokens("'\"");
         assert_eq!(want, got);
     }
 
--- src/token.rs
+++ src/token.rs
@@ -16,39 +16,65 @@ pub enum TokenType {
     Unknown,
     Identifer,
     Plus,
-    Minus,
-    //BANG,
-    //NUMBER,
-    //STRING,
-    //AND,
-    //OR,
-    //EQUALS,
-    //PLUS,
-    //MINUS,
-    //PERIOD,
-    //COMMA,
-    //SLASH,
-    //COLON,
-    //SEMICOLON,
-    //QUESTION,
-    //UNDERSCORE,
-    //PIPE,
-    //AMPERSAND,
-    //PERCENT,
-    //ASTERISK,
-
-    //LEFTPAREN,
-    //RIGHTPAREN,
-    //LEFTANGLE,
-    //RIGHTANGLE,
-    //LEFTBRACKET,
-    //RIGHTBRACKET,
+    Asterisk,
+    Slash,
+    LeftAngle,
+    RightAngle,
+    Equals,
+    Tilde,
+    Bang,
+    At,
+    Hash,
+    Percent,
+    Caret,
+    Ampersand,
+    Pipe,
+    Backtick,
+    Question,
+    Hyphen,
+    Dollar,
+    LeftParen,
+    RightParen,
+    LeftBracket,
+    RightBracket,
+    Comma,
+    Semicolon,
+    Colon,
+    Period,
+    SingleQuote,
+    DoubleQuote,
 }
 
 pub fn tokens() -> HashMap<char, Token> {
     let mut tokens: HashMap<char, Token> = HashMap::new();
 
     tokens.insert('+', Token::new(TokenType::Plus));
-    tokens.insert('-', Token::new(TokenType::Minus));
-    return tokens;
+    tokens.insert('*', Token::new(TokenType::Asterisk));
+    tokens.insert('/', Token::new(TokenType::Slash));
+    tokens.insert('<', Token::new(TokenType::LeftAngle));
+    tokens.insert('>', Token::new(TokenType::RightAngle));
+    tokens.insert('=', Token::new(TokenType::Equals));
+    tokens.insert('~', Token::new(TokenType::Tilde));
+    tokens.insert('!', Token::new(TokenType::Bang));
+    tokens.insert('@', Token::new(TokenType::At));
+    tokens.insert('#', Token::new(TokenType::Hash));
+    tokens.insert('%', Token::new(TokenType::Percent));
+    tokens.insert('^', Token::new(TokenType::Caret));
+    tokens.insert('&', Token::new(TokenType::Ampersand));
+    tokens.insert('|', Token::new(TokenType::Pipe));
+    tokens.insert('`', Token::new(TokenType::Backtick));
+    tokens.insert('?', Token::new(TokenType::Question));
+    tokens.insert('-', Token::new(TokenType::Hyphen));
+    tokens.insert('$', Token::new(TokenType::Dollar));
+    tokens.insert('(', Token::new(TokenType::LeftParen));
+    tokens.insert(')', Token::new(TokenType::RightParen));
+    tokens.insert('[', Token::new(TokenType::LeftBracket));
+    tokens.insert(']', Token::new(TokenType::RightBracket));
+    tokens.insert(',', Token::new(TokenType::Comma));
+    tokens.insert(';', Token::new(TokenType::Semicolon));
+    tokens.insert(':', Token::new(TokenType::Colon));
+    tokens.insert('.', Token::new(TokenType::Period));
+    tokens.insert('\'', Token::new(TokenType::SingleQuote));
+    tokens.insert('"', Token::new(TokenType::DoubleQuote));
+    tokens
 }
