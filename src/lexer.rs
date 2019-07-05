7e7d189eecf2b6c2268e448d7f524366c7473090
Andres Lowrie <andres.lowrie@gmail.com>
2019-07-05 10:01:56 -0500

    Remove prints

--- src/lexer.rs
+++ src/lexer.rs
@@ -16,50 +16,34 @@ fn get_tokens(input: &str) -> Vec<Token> {
 
         match ch {
             Some(t) => {
-                // @TODO change the approach here
                 // It should parse out keywords by consuming chars and identifying when each
                 // keyword or identifer ends
                 //
-                // Something like
-                // Check for single character symbols first if none are found then search for
-                // keywords
                 let mut word = String::from("");
-                if terminators.iter().any(|(k, _)| *k == t.to_string()) {
-                    println!("End of word top level");
-                } else {
-                    word.push(t);
-                    loop {
-                        ch = chars.next();
-                        if let Some(c) = ch {
-                            //println!("Some c from ch");
-                            //print!(" {:?}", c);
-                            if terminators.iter().any(|(k, _)| *k == c.to_string()) {
-                                println!("End of word in loop");
-                                match keywords.get(&word) {
-                                    Some(w) => found_tokens.push(w.clone()),
-                                    None => {
-                                        found_tokens.push(Token::new(TokenType::Identifer(word)))
-                                    }
-                                }
-                                break;
-                            } else {
-                                word.push(c);
-                            }
-                        } else {
-                            // This should catche the case where the token is that last thing in
-                            // the input ie: no new line or extra whitespace
-                            println!("End of nested loop");
-                            println!("{:?}", word);
+
+                word.push(t);
+                loop {
+                    ch = chars.next();
+                    if let Some(c) = ch {
+                        if terminators.iter().any(|(k, _)| *k == c.to_string()) {
                             match keywords.get(&word) {
                                 Some(w) => found_tokens.push(w.clone()),
                                 None => found_tokens.push(Token::new(TokenType::Identifer(word))),
                             }
-                            //print!("word {:?}", word);
                             break;
+                        } else {
+                            word.push(c);
                         }
+                    } else {
+                        // This should catche the case where the token is that last thing in
+                        // the input ie: no new line extra whitespace
+                        match keywords.get(&word) {
+                            Some(w) => found_tokens.push(w.clone()),
+                            None => found_tokens.push(Token::new(TokenType::Identifer(word))),
+                        }
+                        break;
                     }
                 }
-                //println!("word {:?}", word);
             }
             None => {
                 found_tokens.push(Token::new(TokenType::EOF));
